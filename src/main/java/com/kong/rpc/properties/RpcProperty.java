e73c9711478c2256a107ee9b8869e4a0ba97f1a7
kongfq <1028564907@qq.com>
2022-08-28 13:20:52 +0800

    配置

---
+++ src/main/java/com/kong/rpc/properties/RpcProperty.java
@@ -0,0 +1,51 @@
+package com.kong.rpc.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * 参数配置类
+ * @author k
+ */
+@EnableConfigurationProperties(RpcProperty.class)
+@ConfigurationProperties("kong.rpc")
+public class RpcProperty {
+    /**
+     * 服务注册中心
+     */
+    private String registerAddress = "127.0.0.1:2181";
+
+    /**
+     * 服务端暴露端口
+     */
+    private Integer serverPort = 19000;
+
+    /**
+     * 服务协议
+     */
+    private String protocol = "leisure";
+
+    public String getRegisterAddress() {
+        return registerAddress;
+    }
+
+    public void setRegisterAddress(String registerAddress) {
+        this.registerAddress = registerAddress;
+    }
+
+    public Integer getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(Integer serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+}
